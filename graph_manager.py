6c9cfe15fed924e10de7d97712178f513196ec2d
Giga2005 <145823654+Giga2005@users.noreply.github.com>
2025-03-26 22:07:11 +0530

    graph_manager.py

--- graph_manager.py
+++ graph_manager.py
@@ -5,6 +5,6 @@ def detect_deadlock(edges):
     G.add_edges_from(edges)
     try:
         cycle = nx.find_cycle(G, orientation='original')
-        return True, cycle  
+        return True, cycle 
     except nx.NetworkXNoCycle:
         return False, []  
